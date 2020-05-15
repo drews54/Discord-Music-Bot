385cf79ee3346165f565fbb0b55d5c5ac37e28f4
Tony <piunov.doc@yandex.ru>
2020-05-16 01:21:43 +0300

    Converter is working. Several little improvements

--- cogs/music.py
+++ cogs/music.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import youtube_dl
+import subprocess
 from discord.utils import get
 from discord.ext import commands
 from asyncio import run_coroutine_threadsafe
@@ -9,10 +10,11 @@ class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
         self._songlist = []
+        self._filelist = []
         self._unknown_files = 0
         self._music_path = './music/'
         if os.path.exists(self._music_path):
-            self._songlist, self._unknown_files = update_songlist(self._music_path)
+            self._songlist, self._unknown_files = update_songlist()
         else:
             os.mkdir(self._music_path)
 
@@ -31,9 +33,9 @@ class Music(commands.Cog):
             string += f'{i!s}. {name[:-5]!s}\n'
         await self.boxed_print(ctx, string)
         if self._unknown_files == 1:
-            await self.boxed_print(ctx, 'Also there is a file with unknown extension. Check your music folder.')
+            await self.boxed_print(ctx, 'Also there is a file with unknown extension. Use @convert list to convert your music files to "opus" format.')
         elif self._unknown_files > 1:
-            await self.boxed_print(ctx, f'Also there are {self._unknown_files!s} files with unknown extension. Check your music folder.')
+            await self.boxed_print(ctx, f'Also there are {self._unknown_files!s} files with unknown extension. Use @convert list to convert your music files to "opus" format.')
 
     @commands.command(brief = 'Stops playing audio')
     async def stop(self, ctx, loop = ''):
@@ -83,7 +85,7 @@ class Music(commands.Cog):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url)
             await self.boxed_print(ctx, 'Song downloaded: \n' + info['title'])
-        self._songlist, self._unknown_files = update_songlist(self._music_path)
+        self._songlist, self._unknown_files = update_songlist()
         await self.list_(ctx)
 
     @commands.command(brief = 'Removes a song selected from the list')
@@ -94,6 +96,7 @@ class Music(commands.Cog):
                 song = self._songlist.pop(int(number) - 1)
                 os.remove(self._music_path + song)
                 await self.boxed_print(ctx, f'Song {song[:-5]} has been deleted')
+                await self.list_(ctx)
             else:
                 await self.boxed_print(ctx, f'Select an existing song from the list')
 
@@ -107,11 +110,37 @@ class Music(commands.Cog):
             await self.boxed_print(ctx, 'Music folder is now empty')
         self._songlist.clear()
 
-def update_songlist(music_path):
+    @commands.command(brief = 'Converts music file to opus format')
+    async def convert(self, ctx, arg, ext = 'mp3'):
+        if arg == 'list':
+            self._filelist = update_songlist('mp3')[0]
+            if not self._filelist:
+                await self.boxed_print(ctx, 'No files to convert')
+                return
+            i = 0
+            string = ''
+            for name in self._filelist:
+                i += 1
+                string += f'{i!s}. {name!s}\n'
+            string = string + 'Use @convert [number] to convert files from list to "opus" format.'
+            await self.boxed_print(ctx, string)
+        else:
+            file = self._filelist[int(arg) - 1]
+            await self.boxed_print(ctx, f'Performing convertation {file} to ".opus" format...')
+            cmd = f'ffmpeg -i "music/{file}" "music/{file[:-len(ext)]}opus"'
+            subprocess.call(cmd, shell=True)
+            os.remove('./music/' + file)
+            self._songlist.append(f'{file[:-len(ext)]}opus')
+            self._songlist.sort()
+            self._unknown_files = self._unknown_files - 1
+            await self.boxed_print(ctx, 'Converted!')
+            await self.list_(ctx)
+
+def update_songlist(ext = 'opus', music_path = './music/'):
     songlist = []
     unknown_files = 0
     for filename in os.listdir(music_path):
-        if filename.endswith('.opus'):
+        if filename.endswith(ext):
             songlist.append(filename)
         else:
             unknown_files += 1
