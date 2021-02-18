6591b1114d64f1a51142675218991186d6e68592
Tony <piunov.doc@yandex.ru>
2021-02-18 18:52:24 +0300

    Add an option to show an entire list of songs Fixes #42

--- cogs/music.py
+++ cogs/music.py
@@ -23,17 +23,16 @@ class Music(commands.Cog):
         await ctx.send('```' + text + '```')
 
     @commands.command(name = 'list', brief = 'Shows songs list')
-    async def list_(self, ctx, page = 1):
+    async def list_(self, ctx, page = 'all'):
         max_page = math.ceil(len(self._songlist)/10)
         if page == 'all':
-            i = 0
             string = 'Full song list:\n'
-            for name in self._songlist:
-                i += 1
-                string += f'{i!s}. {name[:-5]!s}\n'
-                if len(string) >= 1900:
+            for i, name in enumerate(self._songlist):
+                temp_string = f'{(i + 1)!s}. {name[:-5]!s}\n'
+                if len(string + temp_string) > 2000:
                     await self.boxed_print(ctx, string)
                     string = ''
+                string += temp_string
         else:
             if self._songlist and not 0 < page <= max_page:
                 await self.boxed_print(ctx, f'404 bro, use one of {max_page!s} existing pages')
@@ -41,12 +40,10 @@ class Music(commands.Cog):
             elif not self._songlist:
                 await self.boxed_print(ctx, f'No songs! Use {self.prefix}download to download songs')
                 return
-            i = 0
             string = f'Page {page!s} of {max_page!s}:\n'
-            for name in self._songlist:
-                i += 1
-                if page == (i - 1)//10 + 1:
-                    string += f'{i!s}. {name[:-5]!s}\n'
+            for i, name in self._songlist:
+                if page == (i)//10 + 1:
+                    string += f'{(i + 1)!s}. {name[:-5]!s}\n'
         await self.boxed_print(ctx, string)
         if self._unknown_files == 1:
             await self.boxed_print(ctx, f'Also there is a file with unknown extension. Use {self.prefix}convert list to convert your music files to "opus" format.')
