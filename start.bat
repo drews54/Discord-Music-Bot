0315f31b27379cd3c6fcb78c324e60983a0f4af8
Tony <piunov.doc@yandex.ru>
2021-03-23 20:01:08 +0300

    removed '=' in setx command in start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
 @echo off
 title Letov
-setx DISCORD_TOKEN=PASTE_YOUR_TOKEN_HERE
-setx DISCORD_PREFIXES=TYPE_PREFIXES_HERE
-setx LANG=EN_or_RU
+setx DISCORD_TOKEN PASTE_YOUR_TOKEN_HERE
+setx DISCORD_PREFIXES TYPE_PREFIXES_HERE
+setx LANG EN_or_RU
 echo Starting Discord Music Bot...
 python bot.py
 pause
