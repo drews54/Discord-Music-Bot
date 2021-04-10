4f25310942f5033e5c3631de0426d7168a7d6e28
Andrei Amosov <duha9898@gmail.com>
2021-04-10 15:03:11 +0300

    Updated translations Created gettext.sh script for automated .po files update

---
+++ gettext.sh
@@ -0,0 +1,5 @@
+pygettext3 bot.py cogs/music.py
+msgmerge -U locale/en/LC_MESSAGES/Discord-Music-Bot.po messages.pot
+msgfmt locale/en/LC_MESSAGES/Discord-Music-Bot.po -o locale/en/LC_MESSAGES/Discord-Music-Bot.mo
+msgmerge -U locale/ru/LC_MESSAGES/Discord-Music-Bot.po messages.pot
+msgfmt locale/ru/LC_MESSAGES/Discord-Music-Bot.po -o locale/ru/LC_MESSAGES/Discord-Music-Bot.mo
