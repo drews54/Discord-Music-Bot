c4d7d9fedd59f84c43d3891ad43c61d93e622896
Anton <piunov.doc@gmail.com>
2021-09-16 19:58:09 +0300

    Swaped youtube-dl to yt-dlp

--- cogs/music.py
+++ cogs/music.py
@@ -11,7 +11,8 @@ from asyncio import run_coroutine_threadsafe, CancelledError
 import discord
 from discord.utils import get
 from discord.ext import commands
-from youtube_dl import YoutubeDL
+# from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 from youtube_search import YoutubeSearch
 
 if os.getenv('LANG').casefold().startswith('ru'):
