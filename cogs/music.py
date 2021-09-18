2c4867b227794f786ffa33a32755ecc81eee3a12
piunov1998 <piunov.doc@gmail.com>
2021-09-18 14:16:28 +0300

    Removed comment

--- cogs/music.py
+++ cogs/music.py
@@ -11,7 +11,6 @@ from asyncio import run_coroutine_threadsafe, CancelledError
 import discord
 from discord.utils import get
 from discord.ext import commands
-# from youtube_dl import YoutubeDL
 from yt_dlp import YoutubeDL
 from youtube_search import YoutubeSearch
 
