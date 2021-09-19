c3b490064610bb552f5d2d0019d1beff1bea7699
Anton <piunov.doc@gmail.com>
2021-09-19 19:46:54 +0300

    Added missing URL check in `play` command

--- cogs/music.py
+++ cogs/music.py
@@ -162,6 +162,12 @@ class Music(commands.Cog):
             }
             if arg.startswith('http'):
                 url = arg
+                if not self.url_check(url):
+                    await ctx.send(boxed_string(
+                    _('The provided URL is not allowed. '
+                      'Correlate your URL with supported services domains.\nExample: '
+                      r'https://youtu.be/dQw4w9WgXcQ')))
+                return
             else:
                 searchrequest = arg
                 url = 'https://www.youtube.com' + \
