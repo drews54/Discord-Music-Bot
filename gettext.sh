09c086a8fb702a1447c40b98c069b648e12dc2e4
Andrei Amosov <duha9898@gmail.com>
2021-09-27 03:28:59 +0700

    fix(build scripts): added shebang to gettext.sh script
    Specified usage of a POSIX shell when building locales

--- gettext.sh
+++ gettext.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 pygettext3 bot.py cogs/music.py
 msgmerge -U locale/en/LC_MESSAGES/Discord-Music-Bot.po messages.pot
 msgfmt locale/en/LC_MESSAGES/Discord-Music-Bot.po -o locale/en/LC_MESSAGES/Discord-Music-Bot.mo
